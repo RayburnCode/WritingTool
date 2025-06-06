524cbc0b442ece5c11c9ccca180a7bb778e810a6
RayburnCode <rayburncode@gmail.com>
2025-06-05 17:28:04 -0700

    small

---
+++ wrtiting/api/src/db/collection_pool.rs
@@ -0,0 +1,44 @@
+use sqlx::postgres::PgPoolOptions;
+// or for MySQL: use sqlx::mysql::MySqlPoolOptions;
+use sqlx::{Pool, Postgres}; // or MySql for MySQL
+use dotenv::dotenv;
+use std::env;
+
+pub type DbPool = Pool<Postgres>; // or Pool<MySql> for MySQL
+
+pub async fn create_pool() -> Result<DbPool, sqlx::Error> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    
+    PgPoolOptions::new() // or MySqlPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+}
+
+
+
+
+
+
+
+
+// use sqlx::PgPool;
+// use tokio::sync::OnceCell;
+// use dotenv::dotenv;
+// use std::env;
+
+
+// static DB: OnceCell<PgPool> = OnceCell::const_new();
+
+// pub async fn init_db() -> Result<PgPool, sqlx::Error> {
+//     dotenv().ok();
+//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     let connection_pool = PgPool::connect(&database_url).await?;
+//     sqlx::migrate!("./../migrations").run(&connection_pool).await?;
+//     Ok(connection_pool)
+// }
+
+// pub async fn get_db() -> &'static PgPool {
+//     DB.get_or_init(|| async { init_db().await.expect("Failed to initialize database") }).await
+// }
